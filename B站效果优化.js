65681a744789341896e7df67f583134c4c351f1c
O.k <huang22088399@163.com>
2024-07-19 19:18:49 +0800

    增加快捷键点赞d，同时稍后再看快捷键的功能也会点赞

--- B站效果优化.js
+++ B站效果优化.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         优化B站效果
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  try to take over the world!
 // @author       O.k
 // @match        https://t.bilibili.com/?tab=*
@@ -36,6 +36,9 @@
     let delArray = ['.bili-dyn-version-control__reminding', '.right', '.bili-dyn-version-control__btn'];
     delDOMNode(delArray);
 
+
+
+
     // 调整main区（主内容区）的宽度，由于暂时不太懂怎么引用vue，所以先这样写
     let MainintervalId = setInterval(() => {
         let main = document.getElementsByTagName('main')[0];
@@ -44,6 +47,9 @@
             clearInterval(MainintervalId);
         }
     })
+
+
+
 
     //调大部分区域的字体
     //.bili-rich-text__content defalut:14px
@@ -72,21 +78,37 @@
         }
     }, 100);
 
+
+
+
     //当鼠标悬浮并按下s键时，将那个视频添加到稍后再看
     //.bili-dyn-card-video 视频整体
     //.bili-dyn-card-video__mark 稍后再看按钮
-    let clickEvent = document.createEvent('MouseEvent')
-    clickEvent.initEvent('click', true, true);
+    let clickEvent = new Event('click', { bubbles: true, cancelable: true });
 
     let videoList = [];
     //保存当前悬浮的视频的稍后再看按钮，用来给hoverAndCtrlEvent事件响应函数精确地获取当前悬浮的视频的稍后再看按钮
-    let mark;
+    //v1.1修改为添加稍后再看的同时为视频点赞
+    let mark1;
+    let mark2;
     let key = 's';
     //添加给body的事件，当鼠标悬浮并按下s键时，将鼠标悬浮的那个视频添加到稍后再看
     function hoverAndCtrlEvent(e) {
         console.log('keydown');
         if (e.key == key) {
-            mark.dispatchEvent(clickEvent);
+            mark1.dispatchEvent(clickEvent);
+            if(mark2.getAttribute('class').indexOf('active') == -1)
+            {
+                mark2.click();
+            }
+        }
+    }
+    function like(e)
+    {
+        if(e.key == 'd')
+        {
+            console.log('like');
+            mark2.click();
         }
     }
     setInterval(() => {
@@ -97,11 +119,14 @@
                 //鼠标悬浮且按下s
                 item.addEventListener('mouseover', (e) => {
                     //保存当前悬浮的视频的稍后再看按钮
-                    mark = item.querySelector('.bili-dyn-card-video__mark');
+                    mark1 = item.querySelector('.bili-dyn-card-video__mark');
+                    mark2 = item.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector('.like');
                     document.body.addEventListener('keydown', hoverAndCtrlEvent);
+                    document.body.addEventListener('keydown', like);
                 });
                 item.addEventListener('mouseout', (e) => {
                     document.body.removeEventListener('keydown', hoverAndCtrlEvent);
+                    document.body.removeEventListener('keydown', like);
                 })
             });
         }
